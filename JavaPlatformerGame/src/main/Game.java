7eefa60ad481a719aec323590b6e02513d0a483e
Arda Çabuk <ardacabuk1@hotmail.com>
2025-04-03 19:02:08 +0300

    Player movement fixes, attacking animation added

--- JavaPlatformerGame/src/main/Game.java
+++ JavaPlatformerGame/src/main/Game.java
@@ -1,17 +1,28 @@
 package main;
 
+import entities.Player;
+
+import java.awt.*;
+
 public class Game implements Runnable {
 
 	private GameWindow gameWindow;
 	private GamePanel gamePanel;
 	private Thread gameThread;
 	private final int FPS_SET = 120;
+	private final int UPS_SET = 200;
+	private Player player;
 
 	public Game() {
-		gamePanel = new GamePanel();
+		initClases();
+		gamePanel = new GamePanel(this);
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus();
 		startGameLoop();
+	}
+
+	private void initClases() {
+		player = new Player(200,200);
 	}
 
 	private void startGameLoop(){
@@ -19,25 +30,64 @@ public class Game implements Runnable {
 		gameThread.start();
 	}
 
+	private void update(){
+		player.update();
+	}
+
+	public void render(Graphics g){
+		player.render(g);
+	}
+
 	@Override
 	public void run() {
 		double timePerFrame = (double) 1000000000 / FPS_SET;
-		long lastFrame = System.nanoTime();
-		long now;
+		double timePerUpdate = (double) 1000000000 / UPS_SET;
+
+		long prevTime = System.nanoTime();
+
 		int frames = 0;
+		int updates = 0;
 		long lastCheck = System.currentTimeMillis();
+
+		double deltaU=0;
+		double deltaF=0;
+
 		while (true) {
-			now = System.nanoTime();
-			if(now - lastFrame >= timePerFrame){
-				gamePanel.repaint();
-				lastFrame = now;
-				frames++;
+			long currentTime = System.nanoTime();
+
+
+			deltaU += (currentTime - prevTime) / timePerUpdate;
+
+			if(deltaU >= 1){
+				update();
+				updates++;
+				deltaU--;
 			}
+
+			deltaF += (currentTime - prevTime) / timePerFrame;
+			if(deltaF >= 1){
+				gamePanel.repaint();
+				frames++;
+				deltaF--;
+			}
+
+			prevTime = currentTime;
+
+
 			if(System.currentTimeMillis() - lastCheck >= 1000) {
 				lastCheck = System.currentTimeMillis();
-				System.out.println("FPS: " + frames);
+				System.out.println("FPS: " + frames + " | UPS: " + updates);
 				frames = 0;
+				updates = 0;
 			}
 		}
+	}
+
+	public Player getPlayer(){
+		return player;
+	}
+
+	public void windowLostFocus() {
+		player.resetDirBools();
 	}
 }
--- JavaPlatformerGame/src/main/GameWindow.java
+++ JavaPlatformerGame/src/main/GameWindow.java
@@ -1,19 +1,32 @@
 package main;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 
 public class GameWindow {
-	private JFrame jframe;
+    private final JFrame jframe;
 
-	public GameWindow(GamePanel gamePanel) {
+    public GameWindow(GamePanel gamePanel) {
 
-		jframe = new JFrame();
+        jframe = new JFrame();
 
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jframe.add(gamePanel);
-		jframe.setLocationRelativeTo(null);
-		jframe.setResizable(false);
-		jframe.pack();
-		jframe.setVisible(true);
-	}
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
+        jframe.setResizable(false);
+        jframe.pack();
+        jframe.setVisible(true);
+        jframe.addWindowFocusListener(new WindowFocusListener() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                gamePanel.getGame().windowLostFocus();
+            }
+        });
+    }
 }
